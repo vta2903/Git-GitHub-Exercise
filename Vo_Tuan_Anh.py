8c1a5828fd32c7de483dc59f7ce9dd06e1a9ffe3
38.nguyenhuuthang.7.6@gmail.com <38.nguyenhuuthang.7.6@gmail.com>
2026-09-15 10:18:54 +0700

    Add team message - Nguyen Huu Thang

--- Vo_Tuan_Anh.py
+++ Vo_Tuan_Anh.py
@@ -1,3 +1,4 @@
 print("Hello World from Võ Tuấn Anh - Greetings modified!")
 print("Welcome to my Git & GitHub Practical Assignment!")
 print("We are a great team! - Trần Tuấn Minh")
+print("Student B: Nguyen Huu Thang")
